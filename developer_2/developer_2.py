f1a563475f5c448d641e6bab483b3a1aeb361940
Lichao Huang <lhuang38@rrc.ca>
2025-07-28 10:33:20 -0500

    Added documentation for Book class: issue #2

--- developer_2/developer_2.py
+++ developer_2/developer_2.py
@@ -1,4 +1,11 @@
 class Book:
+    """
+    Book:  Maintains the title and author attributes of a book.
+ 
+    Attributes:
+        _title (str): The title of the book.
+        _author (str): The author of the book.
+    """
 
     def __init__(self, title, author):
         self.__title = title
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from developer_3.developer_3 import Car
 
 def main():
     person_instance = Person("Annie", 22)
-  
+    book_instance = Book("Cat in the Hat", " Dr. Seuss") 
     print(person_instance) 
-
+    print(book_instance) 	
 
 if __name__ == "__main__":
     main()
